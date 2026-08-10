c2d4935da56b244c00e92824c0d3b288d4e86bd4
Krunge9286 <krunge1@arizona.edu>
2026-08-10 14:02:38 +0900

    Update test_mapper function

--- tests/test_mysql_repository.py
+++ tests/test_mysql_repository.py
@@ -1,7 +1,5 @@
 from db.mysql_repository import *
 
-# repo = MysqlRepository()
-# test_kanji = '畳'
 
 repo = MysqlRepository()
 test_row = {
@@ -18,10 +16,13 @@ test_kanji = repo.get_kanji("畳")
 
 def test_mapper():
     kanji = repo.mapper(test_row)
-    assert kanji.form == "畳"
-    assert kanji.meanings == ["tatami mat", "fold"]
-    assert kanji.kunyomi_readings == ["たたみ", "たた.む"]
-    assert kanji.onyomi_readings == ["ジョウ"]
+    assert kanji.form == test_row["form"]
+    assert kanji.meanings == test_row["meanings"].split(";")
+    assert kanji.unicode_value == test_row["unicode_value"]
+    assert kanji.stroke_count == test_row["stroke_count"]
+    assert kanji.kunyomi_readings == test_row["kunyomi_readings"].split(";")
+    assert kanji.onyomi_readings == test_row["onyomi_readings"].split(";")
+    assert kanji.nanori_readings == test_row["nanori_readings"].split(";")
 
 def test_get_kanji():
     assert test_kanji is not None
