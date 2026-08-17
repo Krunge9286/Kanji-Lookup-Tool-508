6a29130869a11827a001d1b724ddb92798f4ce68
Krunge9286 <krunge1@arizona.edu>
2026-08-17 12:53:58 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify, render_template
+from app.services import Services
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+app = Flask(__name__)
+services = Services()
+
+@app.route('/', methods=['GET'])
+def ui() -> str:
+    app.logger.info("ui - Got request")
+    return render_template('kanji_lookup_tool.html')
+
+@app.route("/app/kanji", methods=["POST"])
+def lookup_kanji():
+    data = request.get_json()
+    app.logger.info(f"/app/kanji - Got request: {data}")
+    if not data or not data.get("kanji"):
+        return {"message": "Must provide a kanji"}, 400
+    kanji = services.get_kanji_info(data.get("kanji"))
+    return jsonify(kanji.get_json())
+
+if __name__ == "__main__":
+    app.run(host="localhost")
